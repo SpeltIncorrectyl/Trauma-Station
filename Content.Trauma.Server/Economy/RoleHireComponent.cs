445f44b0319733cb0d929499eefac5491a7fd6d1
SpeltIncorrectyl <speltincorrectyl0001@gmail.com>
2026-01-24 21:24:04 +0000

    work on bodyguard

--- Content.Trauma.Server/Economy/RoleHireComponent.cs
+++ Content.Trauma.Server/Economy/RoleHireComponent.cs
@@ -3,6 +3,7 @@ namespace Content.Trauma.Server.Economy;
 /// <summary>
 /// This person is a role that got hired by someone, like a lawyer.
 /// </summary>
+[RegisterComponent]
 public sealed partial class RoleHireComponent : Component
 {
     /// <summary>
